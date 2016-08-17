ce822b23d0d131258c04a2cc8c84d8c9381d952c
Bhavya Bharti <bhavyab@outlook.com>
2016-08-16 17:07:37 -0700

    submit assignment

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-/* Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
-•The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either  .toUpperCase()  or  .toLowerCase()  used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
-•Useful and descriptive  console.log()  messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
-•Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
-•Add, Commit, Push process is being followed; evident in GitHub that commits are made regularly, and with good commit messages.
-Remember to submit this link on Canvas so that your work can be graded */
-
 'use strict';
 var user = prompt('Hello there!! Would you like to play a game today?');
 console.log('Are you going to play the game ', +user);
